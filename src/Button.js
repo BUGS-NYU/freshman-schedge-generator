fcad39f22562639fe014fbdf19a8af1d0f114ecc
tangym27 <tangym27@gmail.com>
2020-06-05 18:23:09 -0400

    created a fake button

---
+++ src/Button.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import styled from 'styled-components';
+import './App.css';
+
+const StyledButton = styled.button`
+background-color: #90caf9;
+border: none;
+color: black;
+width: 100px;
+height: 35px;
+font-size: 15px;
+border-radius: 5px;
+text-transform: uppercase;
+transition: .2s filter;
+:hover { filter: brightness(75%)}
+`
+// you always pass down a handle and you receive an on
+function Button({onClick, children}) {
+  console.log(onClick);
+  return (
+    <StyledButton onClick={onClick}>
+{children}
+    </StyledButton>
+  );
+}
+
+export default Button;
