0f00bc8df6519291967398a753498954af6794a6
Matthew Apuya <m.apuya19@gmail.com>
2020-06-12 17:34:30 -0500

    Create MajorForm.js
    added MajorForm.js

---
+++ src/MajorForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Button from './Button';
+
+function MajorForm() {
+    const [major, setMajor] = useState("");
+
+    function handleChange(event) {
+        event.preventDefault();
+        setMajor(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        alert(major);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <select onChange={handleChange}>
+                <option value="nothing :(">Choose an option</option>
+                <option value="Computer Science">CS</option>
+                <option value="English">Eng</option>
+                <option value="Chemistry">Chem</option>
+                <option value="Math">Math</option>
+            </select>
+
+            <p>
+                You chose {major}.
+            </p>
+
+            <Button>Submit</Button>
+        </form>
+    );
+}
+
+export default MajorForm;
