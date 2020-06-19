f8cef9e2b4aec9725d1fa612bc58fdd8b963f946
Henry Liu <hliu01@stuy.edu>
2020-06-19 17:18:31 -0400

    changed some more names

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Seminar from "./Seminar";
+import SeminarForm from "./SeminarForm";
 
 const App = () => {
   return (
