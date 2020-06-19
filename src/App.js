ec1a6e07376ec98016d04fb7de4de43e2f55696c
Kariina Altosaar <kariina.altosaar@gmail.com>
2020-06-19 18:31:01 -0400

    Fetching CAS majors (#5)
    * added FetchMajor.js
    
    * Delete Major.js
    
    * updated App.js to reflect namechange
    
    * implemented state and effect hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Majors from './Majors';
 import SeminarForm from "./SeminarForm";
 
 const App = () => {
   return (
     <div className="App">
       <SeminarForm />
+      <Majors></Majors>
     </div>
   );
 };
