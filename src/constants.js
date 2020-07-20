bf4f06dd269c5a888c4f7d3f3eae3bfdda7a52e3
Matthew Apuya <m.apuya19@gmail.com>
2020-07-20 15:00:09 -0500

    minor changes

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const badMajors = new Set(["COHRT", "FYSEM","MAINT","SCHOL","NODEP"]);
-export const badseminars = new Set(["COHRT", "FYSEM","MAINT","SCHOL","NODEP"]);
+export const badSeminars = new Set(["COHRT", "FYSEM","MAINT","SCHOL","NODEP"]);
