8183d0bdb81378108518f428dd410f0b17a1732a
Khanh Nguyen <khanhnguyen99hn@gmail.com>
2020-08-20 12:03:58 -0400

    bugs current deployment

--- src/Table.js
+++ src/Table.js
@@ -1,8 +1,6 @@
 import React from "react";
-import styled from "styled-components";
 
 const Course = ({ course }) => {
-  // convert this to styled components;
   return (
     <>
       <div className="Rtable-cell reg-num Rtable-cell--head">
@@ -22,22 +20,22 @@ const Course = ({ course }) => {
 const Table = ({ courses }) => {
   return (
     <div className="Rtable Rtable--6cols Rtable--collapse">
-      <div className="Rtable-cell reg-num Rtable-cell--head">
+      <div className="Rtable-cell reg-num head">
         <h3>Reg Number</h3>
       </div>
-      <div className="Rtable-cell date">
+      <div className="Rtable-cell date head">
         <h3>Day(s)</h3>
       </div>
-      <div className="Rtable-cell start">
+      <div className="Rtable-cell start head">
         <h3>Start Time</h3>
       </div>
-      <div className="Rtable-cell end">
+      <div className="Rtable-cell end head">
         <h3>End Time</h3>
       </div>
-      <div className="Rtable-cell class-name">
+      <div className="Rtable-cell class-name head">
         <h3>Class</h3>
       </div>
-      <div className="Rtable-cell professor Rtable-cell--foot">
+      <div className="Rtable-cell professor head">
         <h3>Professor</h3>
       </div>
       {Object.entries(courses).map(([key, course]) => {
