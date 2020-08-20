5c07ab65d3ae4c739c9646d565b3ce9844afab63
Khanh Nguyen <khanhnguyen99hn@gmail.com>
2020-08-19 22:20:51 -0400

    Updates

---
+++ precache-manifest.1c91ff1310395a724adb6f0bfe3cd060.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e542775053846289be221c0eaf4bc84",
+    "url": "/freshman-schedge-generator/index.html"
+  },
+  {
+    "revision": "809bdfd5588dee36d514",
+    "url": "/freshman-schedge-generator/static/css/main.6b3bbc1d.chunk.css"
+  },
+  {
+    "revision": "63e32d58f3be0cc52a38",
+    "url": "/freshman-schedge-generator/static/js/2.05dedfdb.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/freshman-schedge-generator/static/js/2.05dedfdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809bdfd5588dee36d514",
+    "url": "/freshman-schedge-generator/static/js/main.533a4736.chunk.js"
+  },
+  {
+    "revision": "f09981607de62c3dd0bc",
+    "url": "/freshman-schedge-generator/static/js/runtime-main.c658d491.js"
+  },
+  {
+    "revision": "57d5eecb4633cf056f77b3bf0385b028",
+    "url": "/freshman-schedge-generator/static/media/logo.57d5eecb.jpg"
+  }
+]);
